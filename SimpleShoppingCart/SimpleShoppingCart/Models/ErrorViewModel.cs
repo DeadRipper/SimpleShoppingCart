5abf7de8818ae821224e345d07467aeaad96be30
Dmytro Makarenko <hardyextrim@gmail.com>
2025-07-27 13:16:04 +0300

    added app

---
+++ SimpleShoppingCart/SimpleShoppingCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleShoppingCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
